d3427063437efedee6b4de8c84777e38a1037110
”Valeriy <valerkaf@mail.ru>
2024-12-21 14:51:53 +0600

    Add main structure, Build pattern (Animal,Barrel,Human) BufferedReader and choice lenght array

--- src/main/java/SortingApplication.java
+++ src/main/java/SortingApplication.java
@@ -55,7 +55,7 @@ public class SortingApplication {
 
                     System.out.println("Тип класса: " + (classChoice == 1 ? "Животные" : classChoice == 2 ? "Бочки" : "Люди"));
                     System.out.println("Длинна элементов массива: " + arrayLength);
-                    System.out.println("Выбор опции: " +choice);
+                    System.out.println("Выбор опции: " + choice);
                     break;
                 case 4:
                     // Sort and search data
