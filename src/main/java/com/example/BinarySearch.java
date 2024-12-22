d241539ddb1f60e98fbc00f726a5b2fcf19440a0
feyour <feyour@gmail.com>
2024-12-22 12:05:21 +0300

    BinarySearch

---
+++ src/main/java/com/example/BinarySearch.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.List;
+
+public class BinarySearch<T extends Comparable<T>> {
+    public int search(List<T> list, T key) {
+        int left = 0;
+        int right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            T result = list.get(mid);
+            if (result.compareTo(key) < 0) {
+                left = mid + 1;
+            } else if (result.compareTo(key) > 0) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
